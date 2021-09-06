0e78a9c4c0e7bb5e05a03babcea2de623f1803c7
Vitor Branco de Miranda <vitorbmiranda@gmail.com>
2021-09-06 16:55:33 -0300

    Fix default age value

--- dogs.js
+++ dogs.js
@@ -1,6 +1,6 @@
 const dogsRepository = () => [{ id: 1, name: 'crystal', age: 1 }, { id: 2, name: 'meepo', age: 5 }]
 const findDogById = (id) => dogsRepository().find(dog => dog.id == id)
-const findDogs = (age = 0) => dogsRepository().filter(dog => dog.age >= age) || []
+const findDogs = (age = 1) => dogsRepository().filter(dog => dog.age >= age) || []
 const validateAgeParameter = (age) => !isNaN(age) && age >= 1
 
 module.exports = { dogsRepository, findDogById, findDogs, validateAgeParameter };
