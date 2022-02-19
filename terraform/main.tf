582f2bffc7b525ebdd44f0973055bcfcb52aa872
Vitor Branco de Miranda <vitorbmiranda@gmail.com>
2022-02-19 13:46:05 -0300

    Fixing AMI on TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "default"
+  ami           = "ami-0ff8a91507f77f867"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "ServiceNameTest"
   }
 }
