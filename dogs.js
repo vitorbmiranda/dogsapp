ddccba032b3651521a1f7e5e020ea13ce5dc5905
Vitor Branco de Miranda <vitorbmiranda@gmail.com>
2022-04-09 11:41:36 -0300

    Fixing minAge parameter

--- dogs.js
+++ dogs.js
@@ -1,6 +1,6 @@
 const dogsRepository = () => [{ id: 1, name: 'crystal', age: 1 }, { id: 2, name: 'meepo', age: 5 }]
 const findDogById = (id) => dogsRepository().find(dog => dog.id == id)
 const findDogs = (minAge = 1) => dogsRepository().filter(dog => dog.age >= minAge) || []
-const validateAgeParameter = (minAge) => !isNaN(minAge) && minAge >= 5
+const validateAgeParameter = (minAge) => !isNaN(minAge) && minAge >= 1
 
 module.exports = { dogsRepository, findDogById, findDogs, validateAgeParameter };
